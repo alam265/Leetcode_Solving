d5d2e12a0f9d658b8b861a173b721a7dd4786c7a
alam265 <mda7095y@gmail.com>
2025-07-10 18:35:10 +0600

    Optimized Solution added

--- Array/Group_anagram.py
+++ Array/Group_anagram.py
@@ -1,15 +1,36 @@
-strs = ["act","pots","tops","cat","stop","hat"] 
+# strs = ["act","pots","tops","cat","stop","hat"] 
 
-res = [] 
+# res = [] 
 
-for i in range(len(strs)):
-    anagrams = [strs[i]]
-    if strs[i] != -1:
-        for j in range(i+1, len(strs)):
-            if strs[j] != -1 and sorted(strs[i]) == sorted(strs[j]):
-                anagrams.append(strs[j])
-                strs[j] = -1
-        res.append(anagrams)
+# for i in range(len(strs)):
+#     anagrams = [strs[i]]
+#     if strs[i] != -1:
+#         for j in range(i+1, len(strs)):
+#             if strs[j] != -1 and sorted(strs[i]) == sorted(strs[j]):
+#                 anagrams.append(strs[j])
+#                 strs[j] = -1
+#         res.append(anagrams)
 
 
-print(res)
+# print(res)
+
+
+"""Optimezed Solution """
+
+strs = ["act","pots","tops","cat","stop","hat"]  
+ 
+res = {} 
+
+for word in strs:
+    count = [0] * 26 
+    for ch in word: 
+        count[ord(ch) - ord("a")] += 1 
+
+    if tuple(count) not in res: 
+        res[tuple(count)] = [word]
+
+    else:
+         res[tuple(count)].append(word)
+
+
+print(res.values())
