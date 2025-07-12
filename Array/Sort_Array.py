080ca41e926c9be4d3643cc3d0d78674e3582a17
alam265 <mda7095y@gmail.com>
2025-07-13 00:41:10 +0600

    Merge Sort

---
+++ Array/Sort_Array.py
@@ -0,0 +1,45 @@
+
+
+def merge(arr, l, m, r): 
+    left = arr[l:m+1]
+    right = arr[m+1: r+1] 
+    i = l 
+    p1 = 0 
+    p2 = 0 
+
+    while p1 < len(left) and p2 < len(right): 
+        if left[p1] <= right[p2]: 
+            arr[i] = left[p1]
+            p1 += 1 
+        else: 
+            arr[i] = right[p2]
+            p2 += 1 
+
+        i += 1 
+
+    while p1 < len(left): 
+        arr[i] = left[p1] 
+        p1 += 1
+        i += 1
+    while p2 < len(right): 
+        arr[i] = right[p2] 
+        p2 += 1
+        i += 1
+        
+
+
+
+
+
+def mergeSort(arr, l, r):
+    if l == r:
+        return arr 
+    m = (l+r) // 2
+    mergeSort(arr, l, m) 
+    mergeSort(arr, m+1, r) 
+    merge(arr, l, m, r)
+    return arr
+
+arr = [5,9,0,34,1,-1] 
+print(mergeSort(arr, 0, len(arr)-1))
+
