efd3b9a84b9a6f34aa57c68e16756f2451d2ddab
alam265 <mda7095y@gmail.com>
2025-07-13 12:52:01 +0600

    Sort Colors

---
+++ Array/Sort_colors.py
@@ -0,0 +1,23 @@
+nums = [1,0,1,2]
+
+l = 0 
+r = len(nums)-1 
+
+i = 0 
+
+while i <= r: 
+    if nums[i] == 0: 
+        nums[i], nums[l] = nums[l], nums[i]
+        i += 1
+        l += 1
+
+    elif nums[i] == 2: 
+        nums[i], nums[r] = nums[r], nums[i] 
+        r -= 1  
+    else: 
+        i += 1
+
+
+print(nums)
+
+
