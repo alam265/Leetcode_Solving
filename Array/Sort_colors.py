e56804e8dd031289a3ab92955ea7aa9d909972f1
alam265 <mda7095y@gmail.com>
2025-07-13 12:52:40 +0600

    Comment

--- Array/Sort_colors.py
+++ Array/Sort_colors.py
@@ -7,12 +7,12 @@ i = 0
 
 while i <= r: 
     if nums[i] == 0: 
-        nums[i], nums[l] = nums[l], nums[i]
+        nums[i], nums[l] = nums[l], nums[i]      #swap
         i += 1
         l += 1
 
     elif nums[i] == 2: 
-        nums[i], nums[r] = nums[r], nums[i] 
+        nums[i], nums[r] = nums[r], nums[i]  #swap
         r -= 1  
     else: 
         i += 1
