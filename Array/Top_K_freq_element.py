339182b0157b383f4cb5774999bc768b665586e4
alam265 <mda7095y@gmail.com>
2025-07-13 20:21:35 +0600

    Optimal Solution Added

--- Array/Top_K_freq_element.py
+++ Array/Top_K_freq_element.py
@@ -23,26 +23,29 @@
 
 """Optimal Solution""" 
 
+
+
+def Top_k_elem(nums, k):
+    dic = {} 
+    count_bucket = [[] for i in range(len(nums)+1)] 
+    ans = []
+
+    for n in nums:
+        if n not in dic: 
+            dic[n] = 1 
+        else: 
+            dic[n] += 1 
+
+    for number, count in dic.items(): 
+        count_bucket[count].append(number) 
+
+
+    for i in range(len(count_bucket)-1, 0, -1): 
+        for n in count_bucket[i]: 
+            ans.append(n) 
+            if len(ans) == k:
+                return ans 
+
 nums = [1,1,1,2,2,3] 
 k = 2
-
-dic = {} 
-count_bucket = [[] for i in range(len(nums)+1)] 
-ans = []
-
-for n in nums:
-    if n not in dic: 
-        dic[n] = 1 
-    else: 
-        dic[n] += 1 
-
-for number, count in dic.items(): 
-    count_bucket[count].append(number) 
-
-
-for i in range(len(count_bucket)-1, 0, -1): 
-    for n in count_bucket[i]: 
-        ans.append(n) 
-
-
-print(ans)
+print(Top_k_elem(nums, k))
