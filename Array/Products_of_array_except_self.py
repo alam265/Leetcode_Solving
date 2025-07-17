931626033c2a918f70a66b4634d28d633a9e056f
alam265 <mda7095y@gmail.com>
2025-07-17 19:55:41 +0600

    Optimal Solution Added

--- Array/Products_of_array_except_self.py
+++ Array/Products_of_array_except_self.py
@@ -18,6 +18,7 @@ print(ans)
 class Solution:
     def productExceptSelf(self, nums: List[int]) -> List[int]:
         res = [0] * len(nums) 
+        
         pref = 1 
 
         for i in range(len(nums)): 
