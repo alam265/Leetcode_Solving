31a47e1ddfd867853399a16911a92148148abc8b
alam265 <mda7095y@gmail.com>
2025-07-17 19:55:16 +0600

    Optimal Solution Added

--- Array/Products_of_array_except_self.py
+++ Array/Products_of_array_except_self.py
@@ -11,3 +11,26 @@ for i in range(len(nums)):
     ans.append(product) 
 
 print(ans)
+
+
+"""Optimal Solution"""
+
+class Solution:
+    def productExceptSelf(self, nums: List[int]) -> List[int]:
+        res = [0] * len(nums) 
+        pref = 1 
+
+        for i in range(len(nums)): 
+            res[i] = pref 
+            pref *= nums[i] 
+             
+        postfix = 1 
+
+        for j in range(len(nums)-1, -1, -1):
+            res[j] *= postfix
+            postfix *= nums[j] 
+
+        return res 
+
+
+        
