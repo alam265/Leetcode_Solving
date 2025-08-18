cb204178f37be72315be76092f122455792ec64d
alam265 <mda7095y@gmail.com>
2025-08-18 19:24:17 +0600

    O(n) sokn added

--- Array/first_missing_positive.py
+++ Array/first_missing_positive.py
@@ -1,16 +1,37 @@
 """Naive Approach"""
 
-def first_missing_positive(nums): 
-    for i in range(1, len(nums)+1): 
-        if i not in nums: 
-            return i 
+# def first_missing_positive(nums): 
+#     for i in range(1, len(nums)+1): 
+#         if i not in nums: 
+#             return i 
+#     return i+1 
 
-nums = [-2,-1,0]
+# nums = [-2,-1,0]
 
-print(first_missing_positive(nums))
+# print(first_missing_positive(nums))
 
 
 
+
+class Solution:
+    def firstMissingPositive(self, nums: list[int]) -> int:
+        for i in range(len(nums)):
+            if nums[i] < 0:
+                nums[i] = 0
+
+        for i in range(len(nums)):
+            val = abs(nums[i])
+            if 1 <= val <= len(nums):
+                if nums[val - 1] > 0:
+                    nums[val - 1] *= -1
+                elif nums[val - 1] == 0:
+                    nums[val - 1] = -1 * (len(nums) + 1)
+
+        for i in range(1, len(nums) + 1):
+            if nums[i - 1] >= 0:
+                return i
+
+        return len(nums) + 1
 
 
 
