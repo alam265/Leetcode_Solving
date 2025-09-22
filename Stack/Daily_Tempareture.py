8b970ed442eeed5826e635438623a1718d83ad5c
alam265 <mda7095y@gmail.com>
2025-09-22 19:31:30 +0600

    Daily Tempareture (OptimalSoln)

--- Stack/Daily_Tempareture.py
+++ Stack/Daily_Tempareture.py
@@ -1,17 +1,36 @@
-temperatures = [30,38,30,36,35,40,28] 
+class Solution:
+    def dailyTemperatures(self, temperatures: List[int]) -> List[int]:
+        res = [0]*len(temperatures)
+        stack = [] 
 
-res = [] 
+        for i in range(len(temperatures)): 
+            while stack and stack[-1][0] < temperatures[i]: 
+                val, idx = stack.pop() 
+                diff = i - idx  
+                res[idx] = diff 
 
-for i in range(len(temperatures)):
-    flag = False
-    for j in range(i+1, len(temperatures)): 
-        if temperatures[j] > temperatures[i]: 
-            res.append(j-i)
-            flag = True
-            break 
+            else:
+                stack.append((temperatures[i], i))  
+        return res
+        
+
+
+
+
+# temperatures = [30,38,30,36,35,40,28] 
+
+# res = [] 
+
+# for i in range(len(temperatures)):
+#     flag = False
+#     for j in range(i+1, len(temperatures)): 
+#         if temperatures[j] > temperatures[i]: 
+#             res.append(j-i)
+#             flag = True
+#             break 
             
-    if flag == False:
-        res.append(0)
+#     if flag == False:
+#         res.append(0)
     
 
-print(res)
+# print(res)
