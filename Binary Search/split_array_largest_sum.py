4fe7c24ed673d2630a540c763020c2360223c5f1
alam265 <mda7095y@gmail.com>
2025-11-04 15:01:46 +0600

    code updated

--- Binary Search/split_array_largest_sum.py
+++ Binary Search/split_array_largest_sum.py
@@ -1,24 +1,24 @@
 class Solution:
     def splitArray(self, nums: List[int], k: int) -> int:
         def canSplit(largest):
-            subarray = 1
-            curSum = 0
-            for num in nums:
-                curSum += num
-                if curSum > largest:
-                    subarray += 1
-                    if subarray > k:
-                        return False
-                    curSum = num
-            return True
+            noArr = 1
+            currSum = 0 
+            for n in nums:
+                currSum += n 
+                if currSum > largest:
+                    noArr += 1 
+                    currSum = n 
+            return noArr <= k 
 
+
+        ans = 0
         l, r = max(nums), sum(nums)
-        res = r
-        while l <= r:
-            mid = l + (r - l) // 2
-            if canSplit(mid):
-                res = mid
-                r = mid - 1
+        while l <= r: 
+            mid = (l+r) // 2 
+            if canSplit(mid): 
+                ans = mid 
+                r = mid - 1 
             else:
-                l = mid + 1
-        return res
+                l = mid + 1 
+
+        return ans
