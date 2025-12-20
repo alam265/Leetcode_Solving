aaca8a8a75581ea37af60df0b4518cd03e27adfc
alam265 <mda7095y@gmail.com>
2025-12-21 00:04:56 +0600

    Linked List has cycle or not

---
+++ Linked List/HasCycle.py
@@ -0,0 +1,11 @@
+class Solution:
+    def hasCycle(self, head: Optional[ListNode]) -> bool:
+        p1 = head 
+        p2 = head 
+
+        while p2 and p2.next:
+            p1 = p1.next 
+            p2 = p2.next.next
+            if p1 == p2:
+                return True  
+        return False 
