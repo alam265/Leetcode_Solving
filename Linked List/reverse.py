a4d090ff8275174350a8052a035140cca9c3aef0
alam265 <mda7095y@gmail.com>
2025-12-23 11:23:08 +0600

    Another solution added for Reverse Linked List

--- Linked List/reverse.py
+++ Linked List/reverse.py
@@ -11,3 +11,17 @@ class Solution:
             q.next = r  
 
         return q
+    
+
+"""Using 2 pointers"""
+
+class Solution:
+    def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        p = head 
+        prev = None 
+        while p: 
+            temp = p.next 
+            p.next = prev 
+            prev = p 
+            p = temp 
+        return prev
