99899031e19128c39fba808119bf609c6565dd74
alam265 <mda7095y@gmail.com>
2025-12-23 12:30:39 +0600

    Recusrion Solution Added

--- Linked List/reverse.py
+++ Linked List/reverse.py
@@ -25,3 +25,15 @@ class Solution:
             prev = p 
             p = temp 
         return prev
+    
+
+"""With Recursion"""
+class Solution:
+    def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        if head == None or head.next == None:
+            return head 
+
+        newHead = self.reverseList(head.next) 
+        head.next.next = head 
+        head.next = None 
+        return newHead 
